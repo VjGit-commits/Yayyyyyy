9c393fbfd827c4a2f0c15f3223f72ab4c8b6b4c4
VjGit-commits <viraj171218@gmail.com>
2026-06-30 20:51:32 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: "/Yayyyyyy/",  
-});
+  plugins: [react()],
+  // Changing this to './' makes all your paths relative
+  base: './', 
+})
